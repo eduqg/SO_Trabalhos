2d9e795c63fa7f894863e8581e112ea4f601053c
eduqg <eduardoqgomes@gmail.com>
2016-08-25 11:16:05 -0300

    Improving answer 3

--- T1/3 - Variaveis/variaveis.c
+++ T1/3 - Variaveis/variaveis.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "variaveis.h"
+#include <string.h>
 
 
 int main(){
@@ -7,6 +8,8 @@ int main(){
 	double number1 = 7.3, number2;
 	char s1[100], s2[100];
 
+	float comparation = 0;
+	int size_string_1 = 0;
 	/*
 	Declare a variável dPtr como ponteiro para a variável do tipo double.
 	*/
@@ -39,6 +42,8 @@ int main(){
 	/*
 	O valor impresso decorrente do enunciado que contempla o item anterior é igual ao
 	valor do endereço gravado em dPrt?
+
+	R: Sim, o valor é igual.
 	*/
 
 	/*
@@ -46,15 +51,41 @@ int main(){
 	s2. Compare a string armazenada em s1 com a string armazenada em s2 e imprima
 	o resultado na tela.
 	*/
+	printf("Digite uma string: ");
+	scanf("%s", s1);
+	strcpy(s2,s1);
+	printf("s1: %s\n", s1);
+	printf("s2: %s\n", s2);
+
+	comparation = strcmp(s1,s2);
+	if(comparation == 0)
+	{
+		printf("As strings são iguais\n");
+	}
+	else
+	{
+		printf("As strings são diferentes\n");
+	}
 
 	/*
-	Apense a string s2 à string s1. Imprima o resultado na tela.*/
+	Apense a string s2 à string s1. Imprima o resultado na tela.
+	Apensar: Juntar
+	*/
+
+	strcat(s2,s1);
+	printf("s2: %s\n", s2);
+
 	/*
 	A execução do item anterior pode provocar algum erro em tempo de execução?
+
+	R: Pode, se a junção das duas strings superar o valor alocado de s2.
 	*/
 	/*
 	Determine o comprimento da string armazenada em s1 e imprima o resultado na
 	tela.
 	*/
+	size_string_1 = strlen(s1);
+	printf("O tamanho da string 1 é: %d\n", size_string_1);
+
 	return 0;
 }
