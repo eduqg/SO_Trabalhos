9afc393bd294d71fbddc7b0fd28da78884471f87
Miguel <miguelpimentel2013@gmail.com>
2016-08-31 13:45:30 -0300

    Inserting question 1 in T2

---
+++ T2/1/alarme.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+
+/* More info : http://stackoverflow.com/questions/9289234/c-process-fork-and-sigalarm */
+
+int main(void)
+{
+	/*
+		fork()  creates  a new process by duplicating the calling process. 
+		The new process is referred to as the child process.
+	*/
+	int PID = fork();
+
+
+	if(PID == 0)
+	{
+		//child process
+		printf("Child process\n");
+		sleep(5);
+		/*
+
+		*/
+		kill(PID, SIGALRM);
+	} 
+	else if(PID == -1)
+	{
+		//error has ocurred
+
+		printf("Process failed\n");
+	}
+	else
+	{
+
+		//parent process
+		pause();
+	}
+
+	return 0;
+}
