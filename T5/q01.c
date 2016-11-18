59999b89856fb4b7856baeebea9d29ecfda9cd83
miguelpimentel <miguelpimentel2013@gmail.com>
2016-11-18 15:40:34 -0200

    Inserting partial answer to T5

--- T5/q01.c
+++ T5/q01.c
@@ -1,41 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <unistd.h>
 
 pthread_mutex_t lock; 
+pthread_t* threads;
 
-void method(int number);
+int isOK = 1;
+
+int number_of_threads = 0;
+
 void destroy(pthread_t* threads);
-void* func(void* ptr);
+void* function(void *ptr);
 
 int main(int argc, char** argv)
 {
-
-	const int number_of_threads = atoi(argv[1]);
+	number_of_threads = atoi(argv[1]);
 
 	if(argc != 2 || number_of_threads > 10 || number_of_threads == 0)
 	{
 		printf("Usage: %s <number_of_threads>\n\n", argv[0]);
-		printf("Ps: The number of threads must be grater than 1 and smaller than 10.\n");
+		printf("Ps: The number of threads must be grater than 0 and smaller than or equal to 	10.\n");
 		exit(1);
 	}	
-		
+	else
+		//NOTHING TO DO
 
-
-
-
-	pthread_t *threads;
-
+	//Handler to control when to operation should stop
+	//signal(SIGINT, handler);
+	
 	threads = (pthread_t *) calloc(number_of_threads, sizeof(pthread_t));
 
+	int max_number_of_inputs[10];
 
-	for(int  i = 0; i < number_of_threads; ++i)
-		threads[i] = pthread_create(&threads[i], NULL, &method, );
+	for(int i = 1; i < 10 ; ++i)
+		max_number_of_inputs[i] = i;
+
+	for(int i = 0; i < number_of_threads; ++i)
+		pthread_create(&threads[i], NULL, function, &max_number_of_inputs[i]);
 
 
-
-	method(number_of_threads);
-
+	while(isOK);
 
 
 	destroy(threads);
@@ -49,18 +54,30 @@ void destroy(pthread_t* threads)
 	free(threads);
 }
 
-void method(int number)
+
+void* function(void *ptr)
 {
-	char aux = ' '; 
+	int *index = (int *) ptr;
+	
+	while(isOK)
+	{
+		//Similar to Sleep. The params are in microsecond	
+	 	usleep(500000);
 
-	for(int i = 96; i <= 96 + number; ++i)	
-		aux =  i;
+	 	//Lock a thread using a mutex
+	 	pthread_mutex_lock(&lock);
 
-	for(int i = 0; i < number; ++i)
-	{	
-		if(i != number - 1)
-			printf("%c ", aux);
-		else
-			printf("%c \n", aux);
+	 	//Print acord to ascci table
+	 	for(int i = 97; i <= *index + 97; ++i)
+	 		printf("%c ", i);
+
+	 	printf("\n");
+
+ 		//Unlock a thread using a mutex
+	 	pthread_mutex_unlock(&lock);
 	}
+
+ 	//Function that terminates the calling thread
+	pthread_exit(0);
 }
+
