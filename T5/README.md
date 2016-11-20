99ed4a26d56aaab92b6f7474fab51bc90b674e9e
miguelpimentel <miguelpimentel2013@gmail.com>
2016-11-20 15:11:27 -0200

    Inserting counting of printed lines

---
+++ T5/README.md
@@ -0,0 +1,17 @@
+Trabalho 5 de Fundamentos de Sistemas Operacionais
+
+Para utilizar o software abra um terminal com ctrl + alt + t.
+
+Para compilar e montar Digite:
+
+make
+
+Para executar:
+
+./q01 <number_of_threads>
+
+Vale ressaltar que o número de mínino de threads para funcionamento deve ser 1 e o número máximo de threads deve ser 10.
+
+Para excluir o executável:
+
+make clean 
--- T5/q01.c
+++ T5/q01.c
@@ -1,89 +1,139 @@
+/*============================================================================*/
+/*Name        : q01.c*/
+/*Author      : Eduardo Gomes and Miguel Pimentel*/
+/*Version     :*/
+/*Copyright   :*/
+/*Description : Program that shoots n thrads( 1, 10) and display the position of character correspondent to alphabet*/
+/*============================================================================*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <signal.h>
 #include <unistd.h>
 
-pthread_mutex_t lock; 
-pthread_t* threads;
+typedef struct Args ARGS;
 
-int isOK = 1;
-
-int number_of_threads = 0;
-
-void destroy(pthread_t* threads);
-void* function(void *ptr);
-
-int main(int argc, char** argv)
+struct Args
 {
-	number_of_threads = atoi(argv[1]);
-
-	if(argc != 2 || number_of_threads > 10 || number_of_threads == 0)
-	{
-		printf("Usage: %s <number_of_threads>\n\n", argv[0]);
-		printf("Ps: The number of threads must be grater than 0 and smaller than or equal to 	10.\n");
-		exit(1);
-	}	
-	else
-		//NOTHING TO DO
+    char character;
+    int printed_lines;
+    int index;
+};
 
 
-	//IT HAS NOT DONE
-	//Handler to control when to operation should stop
-	//signal(SIGINT, handler);
-	
-	threads = (pthread_t *) calloc(number_of_threads, sizeof(pthread_t));
-
-	int max_number_of_inputs[10];
-
-	//Set all possible inputs (a - j equal to 97 106 in ascii table) 
-	for(int i = 1; i < 10 ; ++i)
-		max_number_of_inputs[i] = i;
+void stop_threads(int aux);
+void* print_thread(void* args);
+char* thread_output(int index, char value);
+void destroy(pthread_t* threads, ARGS* args);
+void print_stats(pthread_t* threads, int number_of_threads, ARGS* args);
 
 
-	//Create threads trhough terminal inputs
-	for(int i = 0; i < number_of_threads; ++i)
-		pthread_create(&threads[i], NULL, function, &max_number_of_inputs[i]);
+pthread_mutex_t lock;
+int is_actived = 1;
 
 
-	//Just a while 1. It should stop with ctrl + c signal (SIGINT, handle)
-	while(isOK);
+int main(int argc, char **argv)
+{
+    int number_of_threads = atoi(argv[1]);
 
+    if(argc != 2 || number_of_threads > 10 || number_of_threads == 0)
+    {
+        printf("Usage: %s <number_of_threads>\n\n", argv[0]);
+        printf("Ps: The number of threads must be grater than 0 and smaller than or equal to 10.\n");
+        exit(1);
+    }
 
-	destroy(threads);
+    pthread_t* threads = (pthread_t *) calloc(number_of_threads, sizeof(pthread_t));
+    ARGS* args = (ARGS *) calloc(number_of_threads, sizeof(ARGS));
 
-	return 0;
+    //Send a signal SIGINT and stop application with a ctrl + c
+    signal(SIGINT, stop_threads);
+    
+    for(int i = 0; i < number_of_threads; ++i)
+    {         
+        args[i].character = 'a' + i;        
+        args[i].index = i;
+
+        pthread_create(&(threads[i]), NULL, &print_thread, &args[i]); 
+    }
+
+    print_stats( threads, number_of_threads, args);
+
+    destroy(threads, args);
+
+    return 0;
 }
 
 
-void destroy(pthread_t* threads)
+void print_stats(pthread_t* threads, int number_of_threads, ARGS* args)
 {
-	free(threads);
+
+    for(int i = 0; i < number_of_threads; ++i)
+         pthread_join(threads[i], NULL);
+
+    printf("Aplicação encerrada com sucesso!\n");
+    printf("Estatísticas:\n");
+
+    for(int i = 0; i < number_of_threads; ++i)
+        printf("thread %d: %d linhas\n", i + 1, args[i].printed_lines);
+
 }
 
-
-void* function(void *ptr)
+void* print_thread(void* args)
 {
-	int *index = (int *) ptr;
-	
-	while(isOK)
-	{
-		//Similar to Sleep. The params are in microsecond	
-	 	usleep(500000);
+    ARGS* aux = (ARGS *) args;
 
-	 	//Lock a thread using a mutex
-	 	pthread_mutex_lock(&lock);
+    int printed_line = aux -> printed_lines;
+    int index =  aux -> index;
+    char c = aux -> character;
 
-	 	//Print acord to ascci table
-	 	for(int i = 97; i <= *index + 97; ++i)
-	 		printf("%c ", i);
+    while(is_actived)
+    {
+        //Lock a thread using a mutex
+        pthread_mutex_lock(&lock);
+        
+        if(!is_actived)
+        {
+            pthread_mutex_unlock(&lock);
+            break;
+        }
 
-	 	printf("\n");
+        printf("%s\n", thread_output(index, c));       
+        usleep(500000);
 
- 		//Unlock a thread using a mutex
-	 	pthread_mutex_unlock(&lock);
-	}
+        //Unlock a thread using a mutex
+        pthread_mutex_unlock(&lock);        
+        
+        usleep(1);
+        printed_line++;
+    }
 
- 	//Function that terminates the calling thread
-	pthread_exit(0);
+    aux -> printed_lines = printed_line;
+
+    return args;
+}
+
+void stop_threads(int aux)
+{
+    printf("Encerrando a aplicação. Aguardando finalização de threads....\n");
+    is_actived = 0;
+}
+
+char* thread_output(int index, char value)
+{
+    char *thread_expression;
+    thread_expression = (char *) calloc((index + 1), sizeof(char));
+    
+    for(int i = 0; i < index + 1; ++i)
+        thread_expression[i] = value;
+     
+    return thread_expression;
+}
+
+void destroy(pthread_t* threads, ARGS* args)
+{
+    free(threads);        
+    free(args);
 }
 
